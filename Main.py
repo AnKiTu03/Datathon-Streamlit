1da6d7717962cbdd931f1cc25748ecf6d9aaf8ca
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-18 18:11:44 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import folium
-from folium.plugins import MarkerCluster
-from streamlit_folium import folium_static
 from sklearn.cluster import KMeans
-import joblib
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
 from s3 import read_files_from_s3
-import google.generativeai as genai
-from pandasai import SmartDatalake
-import requests
 from streamlit_option_menu import option_menu
 import streamlit_shadcn_ui as ui
-from video import video_main
+import streamlit.components.v1 as components
 from feedback import feedback_main
+
+def embed_iframe(url, height=800, width=1200):
+    components.iframe(url, height=height, width=width, scrolling=False)
+
+st.set_page_config(page_title="Crime Analysis Dashboard", page_icon=":chart_with_upwards_trend:", layout="wide")
 
 # Load environment variables
 load_dotenv()
@@ -28,33 +26,20 @@ pandasai_api_key = os.getenv('PANDASAI_API_KEY')
 os.environ['PANDASAI_API_KEY'] = pandasai_api_key
 rapidapi_key = os.getenv('RAPIDAPI_KEY')
 
-# Configure Google Generative AI
-genai.configure(api_key=gen_api)
-generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 64,
-    "max_output_tokens": 8192,
-    "response_mime_type": "text/plain",
-}
-safety_settings = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-]
+
 
 @st.cache_data()
 def load_data():
-    return read_files_from_s3(bucket_name, file_keys)
+    with st.spinner("Loading data..."):
+        return read_files_from_s3(bucket_name, file_keys)
 
 data_frames = load_data()
 accused_data = data_frames['Accused_Data.csv']
 fir_data = data_frames['FIR_Details_Data.csv']
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ['DashBoard', 'MapView', 'Video Analysis', 'Forecast', 'chatbot', 'Feedback'],
-                           icons=['bar-chart', 'radar', 'camera-reels', 'graph-up-arrow', 'chat-left-dots-fill'], 
+    selected = option_menu("Main Menu", ['DashBoard', 'MapView', 'Video Analysis', 'Forecast', 'Victim Analysis', 'Chatbot', 'Feedback'],
+                           icons=['bar-chart', 'radar', 'camera-reels', 'graph-up-arrow', 'users', 'chat-left-dots-fill', 'pencil-square'], 
                            menu_icon="cast", default_index=0, 
                            styles={
                                "icon": {"font-size": "24px"}, 
@@ -99,53 +84,60 @@ if selected == 'DashBoard':
     fig_city_profession = px.bar(city_profession_counts, x='District_Name', y='Count', color='Profession',
                                  title='City wise Profession Distribution',
                                  labels={'District_Name': 'City', 'Count': 'Occurrences', 'Profession': 'Profession'},
-                                 height=500, width=500)
+                                 height=500, width=600)
     fig_city_profession.update_layout(showlegend=True)
 
     city_caste_counts = df.groupby(['District_Name', 'Caste']).size().reset_index(name='Count')
     fig_city_caste = px.bar(city_caste_counts, x='District_Name', y='Count', color='Caste',
                             title='City wise Caste Distribution',
                             labels={'District_Name': 'City', 'Count': 'Occurrences', 'Caste': 'Caste'},
-                            height=500, width=500)
+                            height=500, width=600)
     fig_city_caste.update_layout(showlegend=True)
 
-    st.plotly_chart(fig_heatmap)
-    st.plotly_chart(fig_city_profession)
-    st.plotly_chart(fig_city_caste)
+    with st.spinner("Generating heatmap..."):
+        st.plotly_chart(fig_heatmap)
+
+    with st.spinner("Generating city profession distribution chart..."):
+        st.plotly_chart(fig_city_profession)
+
+    with st.spinner("Generating city caste distribution chart..."):
+        st.plotly_chart(fig_city_caste)
 
 # MapView Section
 elif selected == 'MapView':
-    def apply_kmeans(df, n_clusters=10):
-        kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-        df['cluster'] = kmeans.fit_predict(df[['Latitude', 'Longitude']])
-        return df, kmeans.cluster_centers_
+    st.title('Map View')
+    iframe_src = "http://ksp-data.s3-website-us-east-1.amazonaws.com"
+    embed_iframe(iframe_src, height=800, width=1200)
 
-    def visualize_clusters(df, centers):
-        map_center = [df['Latitude'].mean(), df['Longitude'].mean()]
-        map = folium.Map(location=map_center, zoom_start=12)
-        marker_cluster = MarkerCluster().add_to(map)
+# Video Analysis Section
+elif selected == 'Video Analysis':
+    st.title("Video Analysis")
+    st.components.v1.iframe("https://video-crime.streamlit.app/?embed=true&embed_options=disable_scrolling", height=800, width=1200)
 
-        for _, row in df.iterrows():
-            folium.Marker(
-                location=[row['Latitude'], row['Longitude']],  # Fixed here: Access columns separately
-                icon=folium.Icon(icon='record', color='red'),
-            ).add_to(marker_cluster)
+# Forecast Section
+elif selected == 'Forecast':
+    st.title('Crime Forecast')
+    st.write("Forecasting content goes here.")
+    
+    # Embed external content using iframe
+    st.components.v1.iframe("https://forecast-ksp.streamlit.app/?embed=true&embed_options=disable_scrolling", height=800, width=1200)
 
-        for center in centers:
-            folium.Marker(
-                location=center,
-                icon=folium.Icon(icon='star', color='blue'),
-                popup='Patrol Center'
-            ).add_to(map)
+# Victim Analysis Section
+elif selected == 'Victim Analysis':
+    st.title('Victim Analysis')
+    st.write("Victim Analysis content goes here.")
+    
+    # Embed external content using iframe
+    st.components.v1.iframe("https://victim-ksp.streamlit.app/?embed=true&embed_options=disable_scrolling", height=800, width=1200)
 
-        return map
+# Chatbot Section
+elif selected == 'Chatbot':
+    st.title('Crime Chatbot')
+    st.write("Chatbot content goes here.")
+    
+    # Embed external content using iframe
+    st.components.v1.iframe("https://chatbot-ksp.streamlit.app/?embed=true&embed_options=disable_scrolling", height=800, width=1200)
 
-    st.title('Patrolling Map')
-    fir_data.dropna(subset=['Latitude', 'Longitude'], inplace=True)
-    df = fir_data.drop_duplicates(subset=['Latitude', 'Longitude', 'CrimeHead_Name'])
-    clusters = st.slider('Select number of clusters', min_value=3, max_value=20, value=10, step=1)
-    df, centers = apply_kmeans(df, clusters)
-    crime_map = visualize_clusters(df, centers)
-    folium_static(crime_map)
-
-
+# Feedback Section
+elif selected == 'Feedback':
+    feedback_main()
