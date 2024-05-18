300d948d0ad6fe583d831f813201a974ebbd45db
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-18 22:50:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+from st_on_hover_tabs import on_hover_tabs
 from feedback import feedback_main
 from PIL import Image
 
@@ -22,46 +22,44 @@ def embed_iframe(url):
     st.markdown(iframe_code, unsafe_allow_html=True)
 
 st.set_page_config(page_title="Crime Analysis Dashboard", page_icon=":chart_with_upwards_trend:", layout="wide")
+st.header("Crime Analysis Dashboard")
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
+
 image = Image.open("1.jpg")
 image = image.resize((1200, 200))  # Set custom width and height
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ['DashBoard', 'Forecast', 'Patrolling', 'Video Analysis', 'Victim Analysis', 'Map Analysis', 'Chatbot', 'Feedback'],
-                           icons=['bar-chart', 'graph-up-arrow', 'radar', 'camera-reels', 'person-bounding-box', 'globe-central-south-asia', 'chat-left-dots-fill', 'pencil-square'], 
-                           menu_icon="cast", default_index=0, 
-                           styles={
-                               "icon": {"font-size": "24px"}, 
-                               "nav-link": {"font-size": "20px", "text-align": "left", "margin": "0px",  "--hover-color": "#48A6EE", "margin-top": "10px"},
-                               "nav-link-selected": {"background-color": "#48A6EE", "font-weight": "100"}
-                           })
+    tabs = on_hover_tabs(tabName=['DashBoard', 'Forecast', 'Patrolling', 'Video Analysis', 'Victim Analysis', 'Map Analysis', 'Chatbot', 'Feedback'],
+                         iconName=['dashboard', 'graph-up-arrow', 'radar', 'camera-reels', 'person-bounding-box', 'globe-central-south-asia', 'chat-left-dots-fill', 'pencil-square'],
+                         default_choice=0)
 
-if selected == 'DashBoard':
+if tabs == 'DashBoard':
     st.image(image, use_column_width=True)
     embed_iframe("https://frontpage-ksp.streamlit.app/?embed=true")
 
-elif selected == 'MapView':
+elif tabs == 'MapView':
     st.title('Map View')
     embed_iframe("http://ksp-data.s3-website-us-east-1.amazonaws.com")
 
-elif selected == 'Video Analysis':
+elif tabs == 'Video Analysis':
     st.title("Video Analysis")
     embed_iframe("https://video-crime.streamlit.app/?embed=true")
 
-elif selected == 'Forecast':
+elif tabs == 'Forecast':
     st.title('Crime Forecast')
     embed_iframe("https://forecast-ksp.streamlit.app/?embed=true")
 
-elif selected == 'Victim Analysis':
+elif tabs == 'Victim Analysis':
     st.title('Victim Analysis')
     embed_iframe("https://victim-ksp.streamlit.app/?embed=true")
 
-elif selected == 'Chatbot':
+elif tabs == 'Chatbot':
     st.title('Crime Chatbot')
     embed_iframe("https://chatbot-ksp.streamlit.app/?embed=true")
 
-elif selected == 'Map Analysis':
+elif tabs == 'Map Analysis':
     st.title('Map Analysis')
     embed_iframe("https://map-ksp.streamlit.app/?embed=true")
 
-elif selected == 'Feedback':
+elif tabs == 'Feedback':
     feedback_main()
