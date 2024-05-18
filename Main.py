176b7be700e3bfaf769dd0f18741788f459324cc
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-19 02:55:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://share.streamlit.io/")
+components.iframe("http://ksp-data.s3-website-us-east-1.amazonaws.com/")
