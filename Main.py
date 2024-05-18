a83dbe32c858d118de111971461a74df819394c3
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-19 02:21:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,7 +37,7 @@ elif selected == 'Forecast':
 elif selected == 'Patrolling':
     st.title('Map View')
     st.write("Attempting to load the map view iframe...")
-    iframe_src = "http://ksp-data.s3-website-us-east-1.amazonaws.com/"
+    iframe_src = "http://ksp-data.s3-website-us-east-1.amazonaws.com"
     embed_iframe(iframe_src, height=800, width=1200)
     st.write("If the map does not load, please check the URL and ensure it is correct and accessible.")
 
