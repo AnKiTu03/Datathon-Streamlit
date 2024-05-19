2397acdcdf5ce63a49b7e8f00f4058a43fe31ed9
AnKiTu03 <109533077+AnKiTu03@users.noreply.github.com>
2024-05-19 12:05:26 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,16 +8,29 @@ def embed_iframe(url):
     iframe_code = f'''
     <style>
     .embed-container {{
-        position: fixed;
+        position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
         border: none;
     }}
+    .container {{
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        overflow: hidden;
+    }}
+    .container iframe {{
+        width: 100%;
+        height: 100%;
+        border: none;
+    }}
     </style>
-    <div class='embed-container'>
-        <iframe src="{url}" frameborder="0" allowfullscreen></iframe>
+    <div class="container">
+        <iframe src="{url}"></iframe>
     </div>
     '''
     st.markdown(iframe_code, unsafe_allow_html=True)
